53394498a7f073ecc24058f992a3cec81eccf426
mjvoracek <mjvoracek@gmail.com>
2014-01-07 16:13:32 -0600

    First Commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by Matthew Voracek on 1/7/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Multiply/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  Multiply
+//
+//  Created by Matthew Voracek on 1/7/14.
+//  Copyright (c) 2014 Matthew Voracek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+{
+    
+    __weak IBOutlet UITextField *myNumber;
+    __weak IBOutlet UILabel *myMultiplier;
+}
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
